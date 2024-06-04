0e4ad48cfdd2fc504bbcf5c7e545c0c502cbce90
jramma <pepe.ramos@mortensen.cat>
2024-06-04 21:21:55 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,40 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://jramma.github.io/",
+  base: "/",
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    sitemap(),
+    tailwind(),
+    icon(),
+  ],
 });
