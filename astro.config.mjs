af9fc95df4af954f7cdbc169611ed94704a3a166
jramma <jrmasip097@gmail.com>
2025-02-18 13:04:32 +0100

    ⚙️ react settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://jramma.github.io',
-  base: 'jramma',
-})
+  site: "https://jramma.github.io",
+  base: "home",
+  integrations: [react()],
+  vite: {
+    plugins: [
+      tailwindcss({
+        applyBaseStyles: false,
+      }),
+    ],
+  },
+});
